1ccfe575b8a3103e0ba251d07baa93a5e06b70ee
SMR <seyyedmortezarazavi76@gmail.com>
2021-01-14 23:26:07 +0330

    - added application version. - changed register. - forwarded <argv>  and app instance to QML side.

--- main source/src/main.cpp
+++ main source/src/main.cpp
@@ -1,21 +1,37 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QIcon>
-#include "encodinghandler.h"
+
+#include "subtitlehandler.h"
+#include "qtsingleapplication.h"
+#include "settingmodifier.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+    QtSingleApplication app(argc, argv);
+
+    /// if there is other instance, so data will send to other one.
+    if (app.sendMessage(app.arguments().join('\n'))) {
+        return 0;
+    }
 
     app.setOrganizationName("smr");
+    app.setApplicationVersion("v1");
     app.setOrganizationDomain("smr67.github.io");
-
+    app.setApplicationName("subtitle fixer(pr)");
     app.setWindowIcon(QIcon(":/res/resources/sub_fixer.ico"));
 
-    qmlRegisterType<encodingHandler>("io.encode.handler",1,0,"EncodeHandler");
+    qmlRegisterType<subtitlehandler>("io.subtitle.handler",1,0,"SubtitleHandler");
+    qmlRegisterType<settingModifier>("io.advanced.setting",1,0,"SettingModifier");
 
     QQmlApplicationEngine engine;
+
+    QStringList argvTemp = app.arguments();
+
+    engine.rootContext()->setContextProperty("appInstance", &app);
+    engine.rootContext()->setContextProperty("argv", argvTemp);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
